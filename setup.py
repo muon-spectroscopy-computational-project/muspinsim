a7451b3061242acf83f4dda85f68f485714be915
stur86 <simonesturniolo@gmail.com>
2021-04-30 11:50:06 +0100

    Advanced version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="muspinsim",
-    version="0.3.5",
+    version="0.4.0",
     author="Simone Sturniolo",
     author_email="simonesturniolo@gmail.com",
     description="Full quantum simulation of muon experiments",
