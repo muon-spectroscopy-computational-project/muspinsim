9ce364686ccea7a2106ac4b58ee4e2aeb3117284
stur86 <simonesturniolo@gmail.com>
2021-06-15 11:46:54 +0100

    Made the benchmark a bit more substantial

--- bmark/profileLIntDec.py
+++ bmark/profileLIntDec.py
@@ -5,7 +5,7 @@ from muspinsim.experiment import MuonExperiment
 exp = MuonExperiment(['e', 'mu', 'H'])
 exp.spin_system.add_hyperfine_term(1, np.diag([1,1,4])*100)
 exp.spin_system.add_hyperfine_term(2, np.diag([1,1,4])*100/3)
-exp.set_powder_average()
+exp.set_powder_average(100)
 exp.set_magnetic_field(5.0)
 exp.set_dissipation_coupling(0, 1.0)
 exp.set_muon_polarization('z')
