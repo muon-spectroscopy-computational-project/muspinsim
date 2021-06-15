4614202c53a7143748fa79dbe5efc41490d03147
stur86 <simonesturniolo@gmail.com>
2021-06-15 11:40:57 +0100

    Added benchmarking folder

---
+++ bmark/profileLIntDec.py
@@ -0,0 +1,16 @@
+import cProfile
+import numpy as np
+from muspinsim.experiment import MuonExperiment
+
+exp = MuonExperiment(['e', 'mu', 'H'])
+exp.spin_system.add_hyperfine_term(1, np.diag([1,1,4])*100)
+exp.spin_system.add_hyperfine_term(2, np.diag([1,1,4])*100/3)
+exp.set_powder_average()
+exp.set_magnetic_field(5.0)
+exp.set_dissipation_coupling(0, 1.0)
+exp.set_muon_polarization('z')
+
+times = np.linspace(0, 1, 1000)
+op = exp.spin_system.operator({1: 'z'})
+
+cProfile.run('exp.run_experiment(times, [op], "i")', sort='tottime')
