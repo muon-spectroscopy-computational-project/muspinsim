7bcd070572884ed937880bd43ef1f17358360437
stur86 <simonesturniolo@gmail.com>
2021-10-07 17:53:36 +0100

    Moved version to its own module

--- muspinsim/__init__.py
+++ muspinsim/__init__.py
@@ -2,5 +2,4 @@ from muspinsim.input import MuSpinInput
 from muspinsim.simconfig import MuSpinConfig
 from muspinsim.experiment import ExperimentRunner
 from muspinsim.fitting import FittingRunner
-
-__version__ = "1.0.1"
+from muspinsim.version import __version__
---
+++ muspinsim/version.py
@@ -0,0 +1,5 @@
+"""version.py
+
+Version of the package. Kept separated to allow for import from setup.py"""
+
+__version__ = '1.0.1'
