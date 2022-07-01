fix numpy memory overflow issue
changed logic to calculate result one row at a time to avoid numpy requesting too much memory

muspinsim/hamiltonian.py
@@ -82,17 +82,25 @@ class Hamiltonian(Operator, Hermitian):
 
         # Matrix of evolution operators
         ll = -2.0j * np.pi * (evals[:, None] - evals[None, :])
-        rho = np.exp(ll[None, :, :] * times[:, None, None]) * rho0[None, :, :]
 
-        # Now, return values
+        def calc_single_rho(i):
+            return np.exp(ll[None, :, :] * times[i, None, None]) * rho0[None, :, :]
+
         if len(operators) > 0:
-            # Actually compute expectation values
-            result = np.sum(rho[:, None, :, :] * operatorsT[None, :, :, :], axis=(2, 3))
+            # Actually compute expectation values one at a time
+            result = None
+            for i in range(times.shape[0]):
+                rho = calc_single_rho(i)
+                single_res = np.sum(rho[0, None, :, :] * operatorsT[None, :, :, :], axis=(2, 3))
+                if result is None:
+                    result = single_res
+                else:
+                    result = np.concatenate(([result, single_res]), axis=0)
         else:
-            # Just return density matrices
             sceve = evecs.T.conj()
-            result = [DensityOperator(r, dim).basis_change(sceve) for r in rho]
-
+            for i in range(times.shape[0]):
+                # Just return density matrices
+                result = [DensityOperator(calc_single_rho(i), dim).basis_change(sceve) for i in range(times.shape[0])]
         return result
 
     def integrate_decaying(self, rho0, tau, operators=[]):